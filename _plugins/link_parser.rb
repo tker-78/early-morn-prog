e512ed20438e6eafe585ba39d2a2aa081e2e5306
takuya kinoshita <kktak02@gmail.com>
2022-08-12 23:35:15 +0000

    mod

---
+++ _plugins/link_parser.rb
@@ -0,0 +1,8 @@
+module Jekyll
+   class LinkParser < Liquid::Tag
+      def render(context)
+          "test_link_title"
+      end
+   end
+end
+Liquid::Template.register_tag('link_title', Jekyll::LinkParser)
