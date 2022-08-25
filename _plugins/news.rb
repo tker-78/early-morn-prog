9d011114deee4cd811d0c31944e7e4e8fcbd2d4d
takuya kinoshita <kktak02@gmail.com>
2022-08-26 06:20:51 +0900

    update post and workflow

--- _plugins/news.rb
+++ _plugins/news.rb
@@ -1,14 +1,11 @@
 require 'open-uri'
 require 'json'
-require 'pp'
 
-url = "https://newsapi.org/v2/top-headlines?country=jp&apiKey=b494dadbf4f84d61ba5535455e31b7c5"
-
-req = open(url)
-
-response_body = req.read
+  url = "https://newsapi.org/v2/top-headlines?country=jp&apiKey=b494dadbf4f84d61ba5535455e31b7c5"
+  req = open(url)
+  response_body = req.read
 
 
-File.open("_data/news.json", "w") do |f|
-  f.write response_body
-end
+  File.open("_data/news.json", "w") do |f|
+    f.write response_body
+  end
