c2212e3ae8f907b18efea515d9c841b9115bd35d
takuya kinoshita <kktak02@gmail.com>
2022-08-29 08:22:46 +0900

    change news.rb and add test case

--- lib/news.rb
+++ lib/news.rb
@@ -2,14 +2,58 @@ require 'open-uri'
 require 'json'
 require 'date'
 
-today = Date.today
-url = "https://newsapi.org/v2/top-headlines?category=technology&country=jp&from=#{ (today - 7 ).to_s }&to=#{ today.to_s }&sortBy=popularity&apiKey=b494dadbf4f84d61ba5535455e31b7c5"
-req = open(url)
-response_body = req.read
+# today = Date.today
+# url = "https://newsapi.org/v2/top-headlines?category=technology&country=jp&from=#{ (today - 7 ).to_s }&to=#{ today.to_s }&sortBy=popularity&apiKey=b494dadbf4f84d61ba5535455e31b7c5"
+# req = open(url)
+# response_body = req.read
 
 
-File.open("_data/news.json", "w") do |f|
-  f.write response_body 
+# File.open("_data/news.json", "w") do |f|
+#   f.write response_body 
+# end
+
+
+class News
+  attr_accessor :name, :type, :category, :country, :start_date, :end_date, :url
+
+  def initialize(name='', type='', category='', country='' )
+    @name = name
+    @type = type
+    @category = category
+    @country = country
+    @start_date = start_date
+    @end_date = end_date
+    @url = url
+  end
+
+  def url
+    if category == (nil || '')
+      url = "https://newsapi.org/v2/#{type}&country=#{country}&from=#{start_date}&to=#{ end_date }&sortBy=popularity&apiKey=b494dadbf4f84d61ba5535455e31b7c5"
+    else
+      url = "https://newsapi.org/v2/#{type}?category=#{category}&country=#{country}&from=#{start_date}&to=#{ end_date }&sortBy=popularity&apiKey=b494dadbf4f84d61ba5535455e31b7c5"
+    end
+  end
+
+  def start_date
+    today = Date.today
+    (today -7).to_s
+  end
+
+  def end_date
+    today = Date.today
+    today.to_s
+
+  end
+
+  def save
+    req = open(url)
+    response_body = req.read
+    File.open("_data/#{name}.json", "w") do |f|
+      f.write response_body
+    end
+  end
 end
 
+technology = News.new("technology", "top-headlines", "technology", "jp")
+technology.save
 
--- spec/lib/news_spec.rb
+++ spec/lib/news_spec.rb
@@ -1,8 +1,43 @@
 
 require './lib/news'
 
-RSpec.describe 'Post' do
-  it 'will be true' do 
-    expect(1+1).to eq 2
+RSpec.describe News do
+  let(:today) { Date.today }
+  let(:news) { News.new("technology", "top-headlines", "technology", "jp") }
+  let(:news_blank_category) { News.new("technology", "top-headlines", "", "jp") }
+  it 'has correct attributes' do 
+    expect(news.name).to eq 'technology'
+    expect(news.type).to eq 'top-headlines'
+    expect(news.category).to eq 'technology'
+    expect(news.country).to eq 'jp'
   end
+
+  it 'has correct date' do
+    expect(News.new.start_date).to eq (today - 7 ).to_s
+    expect(News.new.end_date).to eq today.to_s
+  end
+
+  it 'can save json data' do
+    pending 
+  end
+
+  context "url for technology" do
+    it 'has correct url' do
+      expect(news.url).to eq "https://newsapi.org/v2/top-headlines?category=technology&country=jp&from=#{news.start_date}&to=#{news.end_date}&sortBy=popularity&apiKey=b494dadbf4f84d61ba5535455e31b7c5"
+    end
+  end
+
+  context "url for blank category" do
+    it 'has correct url' do
+      expect(news_blank_category.url).to eq "https://newsapi.org/v2/top-headlines&country=jp&from=#{news.start_date}&to=#{news.end_date}&sortBy=popularity&apiKey=b494dadbf4f84d61ba5535455e31b7c5"
+    end
+  end
+
 end
+
+
+
+
+
+
+
