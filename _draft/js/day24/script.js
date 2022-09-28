75a56e1a4e4c459edd091e3233c55732e935e777
takuya kinoshita <kktak02@gmail.com>
2022-09-29 06:54:31 +0900

    update day24

--- _draft/js/day24/script.js
+++ _draft/js/day24/script.js
@@ -2,6 +2,14 @@ document.addEventListener('DOMContentLoaded', function (){
   document.querySelector("#calculate").addEventListener('click', function() {
     let selectedPlanet = document.querySelector("#planet")
     console.log(selectedPlanet.value)
-  })
 
+    const planetImage = document.querySelector("img")
+    console.log(planetImage.src)
+    switch (selectedPlanet) {
+      case 'mercury' :
+       console.log(planetImage.src) 
+    }
+
+
+  })
 })
