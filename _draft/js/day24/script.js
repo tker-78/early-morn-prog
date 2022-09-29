077f5abb7e352827701d9c5b25a793db8b350149
takuya kinoshita <kktak02@gmail.com>
2022-09-30 06:49:19 +0900

    update js day24

--- _draft/js/day24/script.js
+++ _draft/js/day24/script.js
@@ -1,13 +1,29 @@
 document.addEventListener('DOMContentLoaded', function (){
   document.querySelector("#calculate").addEventListener('click', function() {
     let selectedPlanet = document.querySelector("#planet")
-    console.log(selectedPlanet.value)
 
     const planetImage = document.querySelector("img")
-    console.log(planetImage.src)
-    switch (selectedPlanet) {
+    switch (selectedPlanet.value) {
       case 'mercury' :
-       console.log(planetImage.src) 
+        planetImage.src = `${location}images/${selectedPlanet.value}.png`
+      case 'venus' :
+        planetImage.src = `${location}images/${selectedPlanet.value}.png`
+      case 'earth' :
+        planetImage.src = `${location}images/${selectedPlanet.value}.png`
+      case 'moon' :
+        planetImage.src = `${location}images/${selectedPlanet.value}.png`
+      case 'mars' :
+        planetImage.src = `${location}images/${selectedPlanet.value}.png`
+      case 'jupiter' :
+        planetImage.src = `${location}images/${selectedPlanet.value}.png`
+      case 'saturn' :
+        planetImage.src = `${location}images/${selectedPlanet.value}.png`
+      case 'uranus' :
+        planetImage.src = `${location}images/${selectedPlanet.value}.png`
+      case 'neptune' :
+        planetImage.src = `${location}images/${selectedPlanet.value}.png`
+      case 'pluto' :
+        planetImage.src = `${location}images/${selectedPlanet.value}.png`
     }
 
 
