901d1a143320addbd3b7798c135ebc76b43b1475
takuya kinoshita <kktak02@gmail.com>
2022-10-01 07:07:23 +0900

    update js day24

--- _draft/js/day24/script.js
+++ _draft/js/day24/script.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function (){
   document.querySelector("#calculate").addEventListener('click', function() {
     let selectedPlanet = document.querySelector("#planet")
+    let mass = document.querySelector("#mass")
+
+    console.log(mass.value)
 
     const planetImage = document.querySelector("img")
+
+
     switch (selectedPlanet.value) {
       case 'mercury' :
         planetImage.src = `${location}images/${selectedPlanet.value}.png`
@@ -26,6 +31,35 @@ document.addEventListener('DOMContentLoaded', function (){
         planetImage.src = `${location}images/${selectedPlanet.value}.png`
     }
 
+    const desc = document.querySelector(".description strong")
+    desc.textContent = selectedPlanet.value
+   
+    const weightOnPlanet = (planet, mass) => {
+      const gravities = {
+        'mercury': 1,
+        'venus': 2,
+        'earth': 1,
+        'moon': 0.3,
+        'mars': 0.5,
+        'jupiter': 4,
+        'saturn': 6,
+        'uranus': 3,
+        'neptune': 0.8,
+        'plute': 15
+      }
+      return  gravities[planet] * mass
+    }
+
+    const weight = document.querySelector(".weight")
+    weight.textContent = weightOnPlanet(selectedPlanet.value, mass.value)
+    weight.style.color = 'white';
+
+
+ 
+
+
+
 
   })
 })
+
